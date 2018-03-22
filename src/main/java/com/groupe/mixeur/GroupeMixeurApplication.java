8f27fdc15252e66076df135a3ae5973db5e83444
Soulnianfo <snianfo@gmail.com>
2018-03-22 15:18:38 +0100

    first commit

---
+++ src/main/java/com/groupe/mixeur/GroupeMixeurApplication.java
@@ -0,0 +1,12 @@
+package com.groupe.mixeur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupeMixeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupeMixeurApplication.class, args);
+	}
+}
---
+++ src/test/java/com/groupe/mixeur/GroupeMixeurApplicationTests.java
@@ -0,0 +1,16 @@
+package com.groupe.mixeur;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupeMixeurApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
